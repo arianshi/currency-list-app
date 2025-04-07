2f3950ac35fe3617f39658fa8494d5b58f1a73c1
arianshi <arian.shi@outlook.com>
2025-04-07 19:47:23 +0800

    feat: add currency list module

--- screens/CurrencyListScreen/index.tsx
+++ screens/CurrencyListScreen/index.tsx
@@ -1,11 +1,10 @@
-//import React from 'react';
-//import CurrencyListModule from 'react-native-currency-lists';
-//import { useCurrency } from '../../context/currencyContext';
+import React from 'react';
+import CurrencyListModule from 'react-native-currency-lists';
+import { useCurrency } from '../../context/currencyContext';
 
 const CurrencyListScreen = () => {
- //const { data } = useCurrency();
-  return null;
-  // return <CurrencyListModule metadata={{ data }} />;
+  const { data } = useCurrency();
+  return <CurrencyListModule metadata={{ data }} />;
 
 };
 
